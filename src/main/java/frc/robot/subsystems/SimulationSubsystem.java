ba44037706bf1b37377fadeb676ddc651c297afb
ada <cmdada@proton.me>
2025-04-01 14:06:46 -0700

    implemented wpi units library in simulation (prelim)

--- src/main/java/frc/robot/subsystems/SimulationSubsystem.java
+++ src/main/java/frc/robot/subsystems/SimulationSubsystem.java
@@ -5,6 +5,7 @@ import edu.wpi.first.hal.SimDouble;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Translation3d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.networktables.StructArrayPublisher;
 import edu.wpi.first.networktables.StructPublisher;
@@ -19,7 +20,7 @@ public class SimulationSubsystem extends SubsystemBase {
     private Rotation3d m_rotation = new Rotation3d();
     
     // Simulation time step
-    private final double m_deltaTime = 0.02; // 20ms
+    private final double m_deltaTimeSeconds = 0.02; // 20ms
     
     // Publishers for AdvantageScope
     private StructPublisher<Pose3d> m_posePublisher;
@@ -30,11 +31,11 @@ public class SimulationSubsystem extends SubsystemBase {
     private SimDouble m_simulatedGyroAngle;
     
     // Current inputs
-    private double m_simX = 0.0;
-    private double m_simY = 0.0;
-    private double m_simZ = 0.0;
-    private double m_simRotate = 0.0;
-    private double m_simPitch = 0.0;
+    private double m_simXMetersPerSecond = 0.0;
+    private double m_simYMetersPerSecond = 0.0;
+    private double m_simZMetersPerSecond = 0.0;
+    private double m_simRotateRadiansPerSecond = 0.0;
+    private double m_simPitchRadiansPerSecond = 0.0;
     
     public SimulationSubsystem() {
         // Initialize AdvantageScope publishers
@@ -67,20 +68,39 @@ public class SimulationSubsystem extends SubsystemBase {
     }
     
     /**
-     * Set the simulation inputs.
+     * Set the simulation inputs using raw values.
      *
-     * @param x      Forward/backward input
-     * @param y      Left/right input
-     * @param z      Up/down input
-     * @param rotate Yaw rotation input
-     * @param pitch  Pitch rotation input
+     * @param xMetersPerSecond      Forward/backward input (meters per second)
+     * @param yMetersPerSecond      Left/right input (meters per second)
+     * @param zMetersPerSecond      Up/down input (meters per second)
+     * @param rotateRadiansPerSecond Yaw rotation input (radians per second)
+     * @param pitchRadiansPerSecond  Pitch rotation input (radians per second)
      */
-    public void setInputs(double x, double y, double z, double rotate, double pitch) {
-        m_simX = x;
-        m_simY = y;
-        m_simZ = z;
-        m_simRotate = rotate;
-        m_simPitch = pitch;
+    public void setInputs(double xMetersPerSecond, double yMetersPerSecond, double zMetersPerSecond, 
+                         double rotateRadiansPerSecond, double pitchRadiansPerSecond) {
+        m_simXMetersPerSecond = xMetersPerSecond;
+        m_simYMetersPerSecond = yMetersPerSecond;
+        m_simZMetersPerSecond = zMetersPerSecond;
+        m_simRotateRadiansPerSecond = rotateRadiansPerSecond;
+        m_simPitchRadiansPerSecond = pitchRadiansPerSecond;
+    }
+    
+    /**
+     * Set the simulation inputs using different units.
+     *
+     * @param xFeetPerSecond      Forward/backward input (feet per second)
+     * @param yFeetPerSecond      Left/right input (feet per second)
+     * @param zFeetPerSecond      Up/down input (feet per second)
+     * @param rotateDegreesPerSecond Yaw rotation input (degrees per second)
+     * @param pitchDegreesPerSecond  Pitch rotation input (degrees per second)
+     */
+    public void setInputsWithUnits(double xFeetPerSecond, double yFeetPerSecond, double zFeetPerSecond, 
+                                  double rotateDegreesPerSecond, double pitchDegreesPerSecond) {
+        m_simXMetersPerSecond = Units.feetToMeters(xFeetPerSecond);
+        m_simYMetersPerSecond = Units.feetToMeters(yFeetPerSecond);
+        m_simZMetersPerSecond = Units.feetToMeters(zFeetPerSecond);
+        m_simRotateRadiansPerSecond = Units.degreesToRadians(rotateDegreesPerSecond);
+        m_simPitchRadiansPerSecond = Units.degreesToRadians(pitchDegreesPerSecond);
     }
     
     /**
@@ -105,12 +125,16 @@ public class SimulationSubsystem extends SubsystemBase {
         final double dragCoefficientRotational = 2.8; // Rotational drag for yaw (z)
         
         // Convert robot-relative inputs to global frame using the robot's current rotation (yaw)
-        Translation3d robotRelativeForce = new Translation3d(m_simX, m_simY, m_simZ);
+        Translation3d robotRelativeForce = new Translation3d(
+            m_simXMetersPerSecond,
+            m_simYMetersPerSecond,
+            m_simZMetersPerSecond
+        );
         Translation3d globalForce = robotRelativeForce.rotateBy(m_pose.getRotation());
 
         // Calculate rotational force (yaw is unaffected by the translation forces)
-        double rotationalForce = m_simRotate;
-        double pitchForce = m_simPitch;
+        double rotationalForce = m_simRotateRadiansPerSecond;
+        double pitchForce = m_simPitchRadiansPerSecond;
 
         // Apply water resistance (drag force reduces velocity) 
         Translation3d dragForce = new Translation3d(
@@ -123,25 +147,25 @@ public class SimulationSubsystem extends SubsystemBase {
 
         // Update velocity with applied forces and drag (linear and rotational)
         m_velocity = new Translation3d(
-            m_velocity.getX() + (globalForce.getX() + dragForce.getX()) * m_deltaTime,
-            m_velocity.getY() + (globalForce.getY() + dragForce.getY()) * m_deltaTime,
-            m_velocity.getZ() + (globalForce.getZ() + dragForce.getZ()) * m_deltaTime
+            m_velocity.getX() + (globalForce.getX() + dragForce.getX()) * m_deltaTimeSeconds,
+            m_velocity.getY() + (globalForce.getY() + dragForce.getY()) * m_deltaTimeSeconds,
+            m_velocity.getZ() + (globalForce.getZ() + dragForce.getZ()) * m_deltaTimeSeconds
         );
 
         // Update rotation rates with applied forces and drag 
         m_rotation = new Rotation3d(
-            m_rotation.getX() + pitchForce * m_deltaTime,
+            m_rotation.getX() + pitchForce * m_deltaTimeSeconds,
             m_rotation.getY(),
-            m_rotation.getZ() + (rotationalForce + rotationalDragForce) * m_deltaTime
+            m_rotation.getZ() + (rotationalForce + rotationalDragForce) * m_deltaTimeSeconds
         );
 
         // Update pose (position and orientation) 
         m_pose = new Pose3d(
-            m_pose.getTranslation().plus(m_velocity.times(m_deltaTime)),
+            m_pose.getTranslation().plus(m_velocity.times(m_deltaTimeSeconds)),
             new Rotation3d(
-                m_pose.getRotation().getX() + m_rotation.getX() * m_deltaTime,
-                m_pose.getRotation().getY() + m_rotation.getY() * m_deltaTime,
-                m_pose.getRotation().getZ() + m_rotation.getZ() * m_deltaTime
+                m_pose.getRotation().getX() + m_rotation.getX() * m_deltaTimeSeconds,
+                m_pose.getRotation().getY() + m_rotation.getY() * m_deltaTimeSeconds,
+                m_pose.getRotation().getZ() + m_rotation.getZ() * m_deltaTimeSeconds
             )
         );
 
@@ -157,8 +181,18 @@ public class SimulationSubsystem extends SubsystemBase {
         // Update simulated gyro
         if (m_simulatedGyroAngle != null) {
             double currentAngle = m_simulatedGyroAngle.get();
-            double newAngle = currentAngle + m_rotation.getZ() * m_deltaTime * (180 / Math.PI); // Convert radians/s to degrees/s
+            // Convert radians to degrees
+            double rotationInDegrees = Units.radiansToDegrees(m_rotation.getZ() * m_deltaTimeSeconds);
+            double newAngle = currentAngle + rotationInDegrees;
             m_simulatedGyroAngle.set(newAngle);
+        }
+    }
+    
+    @Override
+    public void periodic() {
+        // This method will be called once per scheduler run
+        if (shouldSimulate()) {
+            updateSimulation();
         }
     }
 }
